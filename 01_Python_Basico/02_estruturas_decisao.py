994c01f70e2eef9e2020a77bb41d73053d5be5f7
feat: Iniciando app.py e estrutura. <arthguins@gmail.com>
2024-09-03 18:46:26 -0300

    Exercícios de estruturas de decisão

---
+++ 01_Python_Basico/02_estruturas_decisao.py
@@ -0,0 +1,16 @@
+# Estruturas de decisão: if, elif, else
+# Exercício 1: Verifique se um número é positivo, negativo ou zero
+numero = 0
+if numero > 0:
+    print("Positivo")
+elif numero < 0:
+    print("Negativo")
+else:
+    print("Zero")
+
+# Exercício 2: Verifique se uma pessoa pode votar
+idade = 18
+if idade >= 16:
+    print("Pode votar")
+else:
+    print("Não pode votar")
