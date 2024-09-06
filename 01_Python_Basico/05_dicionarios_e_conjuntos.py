7333d88f60d3068db076c5b642dc0f0495f5f9c7
feat: Iniciando app.py e estrutura. <arthguins@gmail.com>
2024-09-06 18:46:30 -0300

    Exercícios de dicionários e conjuntos

---
+++ 01_Python_Basico/05_dicionarios_e_conjuntos.py
@@ -0,0 +1,8 @@
+# Dicionários e conjuntos
+# Exercício 1: Crie um dicionário com 3 pares chave-valor e imprima as chaves
+aluno = {"nome": "João", "idade": 20, "curso": "Python"}
+print(aluno.keys())
+
+# Exercício 2: Crie um conjunto com números repetidos e imprima o conjunto
+numeros = {1, 2, 2, 3, 4, 4, 5}
+print(numeros)
