985eb6e7e91f8ddb57c3914b9c8111f8cabf301f
feat: Iniciando app.py e estrutura. <arthguins@gmail.com>
2024-09-07 18:46:32 -0300

    Exercícios de funções

---
+++ 01_Python_Basico/06_funcoes.py
@@ -0,0 +1,10 @@
+# Funções
+# Exercício 1: Crie uma função que retorna o quadrado de um número
+def quadrado(x):
+    return x ** 2
+print(quadrado(4))
+
+# Exercício 2: Crie uma função que verifica se um número é par
+def eh_par(n):
+    return n % 2 == 0
+print(eh_par(5))
