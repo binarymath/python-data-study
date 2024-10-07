793005c80f013b931f9d21940e0ad455fe3f470c
feat: Iniciando app.py e estrutura. <arthguins@gmail.com>
2024-10-06 21:15:50 -0300

    Exercícios de dicionários e conjuntos

---
+++ 01_Python_Basico/dicionarios_e_conjuntos.py
@@ -0,0 +1,6 @@
+"""Exercícios com dicionários e conjuntos."""
+# Dicionários e conjuntos
+aluno = {"nome": "João", "idade": 20, "curso": "Python"}
+print(aluno.keys())
+numeros = {1, 2, 2, 3, 4, 4, 5}
+print(numeros)
