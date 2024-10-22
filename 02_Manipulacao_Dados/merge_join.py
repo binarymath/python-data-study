c6155f928da47e8034ad7fa486b1a6d50458a5c4
feat: Iniciando app.py e estrutura. <arthguins@gmail.com>
2024-10-21 21:17:10 -0300

    Merge e Join de DataFrames

---
+++ 02_Manipulacao_Dados/merge_join.py
@@ -0,0 +1,7 @@
+"""Merge e Join de DataFrames."""
+import pandas as pd
+
+df1 = pd.DataFrame({'A': [1, 2], 'B': [3, 4]})
+df2 = pd.DataFrame({'A': [1, 2], 'C': [5, 6]})
+resultado = pd.merge(df1, df2, on='A')
+print(resultado)
